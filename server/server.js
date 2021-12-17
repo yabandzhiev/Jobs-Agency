e9e0715b8851fa3b5796ea2abcd13ade49305cd0
yabandzhiev <yabandzhiev@abv.bg>
2021-12-17 21:01:06 +0200

    process env port

--- server/server.js
+++ server/server.js
@@ -1422,7 +1422,7 @@
 
   const server = http__default["default"].createServer(requestHandler(plugins, services));
 
-  const port = 3030;
+  const port = process.env.PORT || 3030;
   server.listen(port);
   console.log(
     `Server started on port ${port}. You can make requests to http://localhost:${port}/`
